6605618f40f98d569549260e7507a234e06d9201
morningspace <morningspace@yahoo.com>
2020-04-13 12:27:21 +0800

    Update conf.sh

--- conf.sh
+++ conf.sh
@@ -1,7 +1,14 @@
+# The Kubernetes namespace used for this lab
 LAB_NAMESPACE="cp4mcm-lab"
+
+# The CP4MCM URL, usually points to the master node
 CP4MCM_BASE_URL="https://icp-console.10.0.10.2.nip.io"
-AWS_ACCESS_KEY_ID=""
-AWS_SECRET_ACCESS_KEY=""
-AWS_REGION="us-east-2"
-AWS_CLUSTER_NAME="my-cluster-eks"
-KIND_CLUSTER_NAME="my-cluster-kind"
+
+# AWS settings
+AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
+AWS_REGION=${AWS_REGION:-"us-east-2"}
+AWS_CLUSTER_NAME=${AWS_CLUSTER_NAME:-"my-cluster-eks"}
+
+# kind settings
+KIND_CLUSTER_NAME=${KIND_CLUSTER_NAME:-"my-cluster-kind"}
